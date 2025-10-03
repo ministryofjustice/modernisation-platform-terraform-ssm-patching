230b4533d98221100c34a6d881e35776d1958dd9
Dave Kent <dave.kent@justice.gov.uk>
2025-10-03 15:36:22 +0100

    tm-1638-patchmanager-simple-patching-option-rebootifneeded

--- main.tf
+++ main.tf
@@ -110,6 +110,10 @@ resource "aws_ssm_maintenance_window_task" "patch_task" {
           name   = "Operation"
           values = ["Install"]
         }
+        parameter {
+          name   = "RebootOption"
+          values = ["RebootIfNeeded"]
+        }
       }
     }
 
